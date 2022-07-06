a112b033ba3990dee737eb8a2f342eabd8eb75c9
minidiscs <shanedbutler@gmail.com>
2022-07-06 12:01:52 -0400

    click listener function for guests in location

--- scripts/Locations.js
+++ scripts/Locations.js
@@ -1,17 +1,18 @@
-import { getLocations, getSubLocations, getServices, getServicesLocations } from "./database.js";
+import { getLocations, getSubLocations, getServices, getServicesLocations, getGuests } from "./database.js";
 //get locations, publish list of locations in two rows of 3 columns
 
 const locations = getLocations()
 const subLocations = getSubLocations()
 const services = getServices()
 const servicesLocations = getServicesLocations()
+const guests = getGuests()
 
 export const locationsHTML = () => {
     let html = `<section class="locations">`
     let amenityEstablished = 1
 
     for (const location of locations) {
-        html += `<article class="locations--card id="locations--${location.id}"><h2>${location.name}</h2><ul>`
+        html += `<article class="locations--card"><h2 id="locations--${location.id}">${location.name}</h2><ul>`
         for (const subLocation of subLocations) {
             if (subLocation.locationId === location.id) {
                 html += `<li id="subLocation--${subLocation.id}">${subLocation.name}</li>`
@@ -33,4 +34,25 @@ export const locationsHTML = () => {
     html += "</section>"
 
     return html
+}
+
+document.addEventListener("click", (clickEvent) => guestsInLocation(clickEvent))
+
+const guestsInLocation = (clickEvent) => {
+    const itemClicked = clickEvent.target
+    let guestLocationCount = 0
+    if (itemClicked.id.startsWith("locations")) {
+        const [,locationId] = itemClicked.id.split("--")
+
+        for (const location of locations) {
+            if (location.id === parseInt(locationId)) {
+                for (const guest of guests) {
+                    if (guest.locationId === location.id) {
+                        guestLocationCount++
+                    }
+                }
+                window.alert(`${location.name} currently has ${guestLocationCount} guests`)
+            }
+        }
+    }
 }
