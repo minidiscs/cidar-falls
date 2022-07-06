25722f305e66206042af272d7f6033eda8f92974
minidiscs <shanedbutler@gmail.com>
2022-07-06 14:42:45 -0400

    add services function to alert services locations

--- scripts/Services.js
+++ scripts/Services.js
@@ -1,4 +1,5 @@
 import { getLocations, getServices, getServicesLocations } from "./database.js";
+//get services, display at top of page and create click listener that alerts locations which have that service
 
 const locations = getLocations()
 const services = getServices()
@@ -14,4 +15,31 @@ export const serviceHTML = () => {
     }
 
         return html
+}
+
+document.addEventListener("click", (clickEvent) => locationsWithService(clickEvent))
+
+const locationsWithService = (clickEvent) => {
+    const itemClicked = clickEvent.target
+    let locationString = ""
+    if (itemClicked.id.startsWith("service")) {
+        const [,serviceId] = itemClicked.id.split("--")
+        for (const service of services) {
+            if (service.id === parseInt(serviceId)) {
+                for (const servicesLocation of servicesLocations) {
+                    if (service.id === servicesLocation.servicesId) {
+                        for (const location of locations) {
+                            if (location.id === servicesLocation.locationId && !locationString) {
+                                locationString = `${location.name}`
+                            }
+                            else if (location.id === servicesLocation.locationId && locationString != null) {
+                                locationString = `${locationString} and ${location.name}`
+                            }
+                        }
+                    }
+                }
+                window.alert(`${service.name} can be found at ${locationString}`)
+            }
+        }
+    }
 }
